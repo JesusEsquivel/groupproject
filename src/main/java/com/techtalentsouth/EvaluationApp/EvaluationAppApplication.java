8b84f8f77187f148f8364af1ae0add45a833f022
Savannah <savannahlloyd91@gmail.com>
2018-12-14 15:47:58 -0500

    initial commit

---
+++ src/main/java/com/techtalentsouth/EvaluationApp/EvaluationAppApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.EvaluationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationAppApplication.class, args);
+	}
+
+}
+
