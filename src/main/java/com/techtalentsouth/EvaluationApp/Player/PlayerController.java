761486c5406250e3099d7fca626a39201029dce6
Andrew Adkins <adkinsar3@gmail.com>
2018-12-15 19:19:41 -0500

    Full Player list added to Controller and View

--- src/main/java/com/techtalentsouth/EvaluationApp/Player/PlayerController.java
+++ src/main/java/com/techtalentsouth/EvaluationApp/Player/PlayerController.java
@@ -33,5 +33,12 @@ public class PlayerController {
 				"6'0\"", "6'1\"", "6'2\"", "6'3\"", "6'4\"", "6'5\"", "6'6\"", "6'7\"", "6'8\"", "6'9\"", "6'10\"", "6'11\""};
 	}
 	
+	//list all players
+	@GetMapping("/all_players")
+	public ModelAndView getAllPlayers(Player player) {
+		ModelAndView mv = new ModelAndView("player/playerList");
+		mv.addObject("players", playerRepository.findAll());
+		return mv;
+	}
 
 }
